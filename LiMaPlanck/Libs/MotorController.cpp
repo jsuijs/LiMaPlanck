0d4a9a51d40ca527475c66874cba9a39fb62d792
Joep <jsuijs@gmail.com>
2021-05-25 23:58:07 +0200

    Cleanup.

--- LiMaPlanck/Libs/MotorController.cpp
+++ LiMaPlanck/Libs/MotorController.cpp
@@ -1,17 +1,17 @@
 //-----------------------------------------------------------------------------
-// MotorController.cpp
+// MotorController.cpp - the speed-controller of the robot.
 //-----------------------------------------------------------------------------
-// Deze file bevat de snelheidsregeling van de robot.
 //-----------------------------------------------------------------------------
 
-// MotorController (Mc) data structure met PID var's
-struct tMc {
-      int PrevSpeedL, PrevSpeedR; // Vorige snelheid (in mm/sec)
-      int IErrorL, IErrorR;
-   } Mc;
+// MotorController (Mc) data structure with PID var's
+struct tMc
+{
+   int PrevSpeedL, PrevSpeedR; // last speed (in mm/sec)
+   int IErrorL, IErrorR;       // integraded error (I-component of PID)
+} Mc;
 
 //-----------------------------------------------------------------------------
-// SpeedController - regel motorsnelheid
+// SpeedController - control motorspeed
 //-----------------------------------------------------------------------------
 // Parameters: SpeedSoll* (in mm/sec)
 //-----------------------------------------------------------------------------
@@ -21,28 +21,27 @@ void MotorController(int SetpointL, int SetpointR)
    int DError = 999;
    int PowerL, PowerR;
 
-   // reken setpoints om van mm/s naar ticks/interval
+   // convert setpoints from mm/s to ticks/interval
    volatile int SetpointL_Ticks = (SetpointL * 4L * MAIN_TAKT_INTERVAL) / ODO_TICK_TO_METRIC;
    volatile int SetpointR_Ticks = (SetpointR * 4L * MAIN_TAKT_INTERVAL) / ODO_TICK_TO_METRIC;
 
   //CSerial.printf("!! %d %d %d %d\n", SetpointL, SetpointL_Ticks, MAIN_TAKT_INTERVAL, ODO_TICK_TO_METRIC);
 
+   //----------------
+   // Left controller
+   //----------------
    if (SetpointL_Ticks != 0) {
       //  PID motor speed control:
-      PError = SetpointL_Ticks - Position.ActSpeedL;     // P fout, in odo-pulsen per takt-inteval
-      DError = Position.ActSpeedL - Mc.PrevSpeedL;       // D fout
+      PError = SetpointL_Ticks - Position.ActSpeedL;           // P error, in odo-pulsen per takt-inteval
+      DError = Position.ActSpeedL - Mc.PrevSpeedL;             // D error
       Mc.PrevSpeedL = Position.ActSpeedL;
-      Mc.IErrorL += PError;                              // I fout
+      Mc.IErrorL += PError;                                    // I error
 
-      if (Mc.IErrorL >  MPID_I_MAX) {
-         Mc.IErrorL =  MPID_I_MAX;   //anti windup
-      }
-      if (Mc.IErrorL < -MPID_I_MAX) {
-         Mc.IErrorL = -MPID_I_MAX;
-      }
+      if (Mc.IErrorL >  MPID_I_MAX) Mc.IErrorL =  MPID_I_MAX;  //anti windup
+      if (Mc.IErrorL < -MPID_I_MAX) Mc.IErrorL = -MPID_I_MAX;
 
-      //PWM aansturing
-      PowerL = MPID_OFFSET       +                             // PID regeling !!
+      // Calculate PWM
+      PowerL = MPID_OFFSET       +                             // PID controller
                SetpointL_Ticks   * MPID_SP_GAIN +
                PError            * MPID_P_GAIN  +
                Mc.IErrorL        * MPID_I_GAIN  +
@@ -52,24 +51,21 @@ void MotorController(int SetpointL, int SetpointR)
       PowerL     = 0;
    }
 
-   //CSerial.printf("PID_L Perr %d, Ierr %d, Derr %d, Soll %d, Act: %d\n", PError, Mc.IErrorL, DError, SetpointL_Ticks, Position.ActSpeedL);
-
+   //-----------------
+   // Right controller
+   //-----------------
    if (SetpointR_Ticks != 0) {
       //  PID motor speed control:
-      PError = SetpointR_Ticks - Position.ActSpeedR;     // P fout, in odo-pulsen per takt-inteval
-      DError = Position.ActSpeedR - Mc.PrevSpeedR;       // D fout
+      PError = SetpointR_Ticks - Position.ActSpeedR;           // P error, in odo-pulses per takt-inteval
+      DError = Position.ActSpeedR - Mc.PrevSpeedR;             // D error
       Mc.PrevSpeedR = Position.ActSpeedR;
-      Mc.IErrorR += PError;                              // I fout
+      Mc.IErrorR += PError;                                    // I error
 
-      if (Mc.IErrorR >  MPID_I_MAX) {
-         Mc.IErrorR =  MPID_I_MAX;   //anti windup
-      }
-      if (Mc.IErrorR < -MPID_I_MAX) {
-         Mc.IErrorR = -MPID_I_MAX;
-      }
+      if (Mc.IErrorR >  MPID_I_MAX) Mc.IErrorR =  MPID_I_MAX;  //anti windup
+      if (Mc.IErrorR < -MPID_I_MAX) Mc.IErrorR = -MPID_I_MAX;
 
-      //PWM aansturing
-      PowerR = MPID_OFFSET       +                             //PID regeling !!
+      // Calculate PWM
+      PowerR = MPID_OFFSET       +                             // PID controller
                SetpointR_Ticks   * MPID_SP_GAIN +
                PError            * MPID_P_GAIN  +
                Mc.IErrorR        * MPID_I_GAIN  +
@@ -78,6 +74,7 @@ void MotorController(int SetpointL, int SetpointR)
       Mc.IErrorR = 0;
       PowerR     = 0;
    }
+
    Motors(PowerL, PowerR);
 //   CSerial.printf("PID Soll: %d / %d, Ist: %d / %d, I: %d / %d, Power: %d / %d\n",
 //         SetpointL_Ticks, SetpointR_Ticks, Position.ActSpeedL, Position.ActSpeedR, Mc.IErrorL, Mc.IErrorR, PowerL, PowerR);
