bea30eec3745923ed3854203e9a5ec79b4244a46
Joep <jsuijs@gmail.com>
2022-01-03 22:09:12 +0100

    Fix for path with spaces.

--- LiMaPlanck/ArduinoSettings.bat
+++ LiMaPlanck/ArduinoSettings.bat
@@ -7,5 +7,5 @@ rem zie compiler output, zoek op -fqbn (vanaf 'fqbn' tot 1e spatie overnemen)
 set fqbn=STMicroelectronics:stm32:GenF1:pnum=MAPLEMINI_F103CB,upload_method=swdMethod,xserial=none,usb=none,xusb=FS,opt=osstd,rtlib=nano
 
 rem stel configuratie in
-%arduino_path%\arduino_debug --board %fqbn% --save-prefs
+"%arduino_path%\arduino_debug" --board %fqbn% --save-prefs
 pause
