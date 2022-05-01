ad101c71ebebe44981f572cbbc67a4283a07d61e
Joep <jsuijs@gmail.com>
2022-05-01 09:00:57 +0200

    MotorController refactor.

--- LiMaPlanck/Libs/MotorController.cpp
+++ LiMaPlanck/Libs/MotorController.cpp
@@ -3,80 +3,53 @@
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 
-// MotorController (Mc) data structure with PID var's
-struct tMc
+class TMc   // local MotorControl class.
 {
-   int PrevSpeedL, PrevSpeedR; // last speed (in mm/sec)
-   int IErrorL, IErrorR;       // integraded error (I-component of PID)
-} Mc;
+   public:
+      void Compute(int Setpoint, int ActSpeed)
+      {
+         int Setpoint_Ticks = (Setpoint * (long) MAIN_TAKT_INTERVAL) / (F_ODO_TICK_TO_METRIC * 1024);
+
+         if (Setpoint_Ticks == 0) {
+            // Stop
+            IError = 0;
+            Power  = 0;
+            return;
+         }
+
+         //  Moving: PID motor speed control
+         int PError = Setpoint_Ticks - ActSpeed;         // P error, in odo-pulsen per takt-inteval
+         int DError = ActSpeed - PrevSpeed;              // D error
+         PrevSpeed  = ActSpeed;
+         IError += PError;                               // I error
+
+         IError = Clip(IError, -MPID_I_MAX, MPID_I_MAX); // Integrator anti windup
+
+         // Calculate PWM using PID + proportional forward
+         Power =  MPID_OFFSET       +                    // forward offset
+                  Setpoint_Ticks    * MPID_SP_GAIN +     // forward gain (proportional)
+                  PError            * MPID_P_GAIN  +     // P
+                  IError            * MPID_I_GAIN  +     // I
+                  DError            * MPID_D_GAIN  ;     // D
+      }
+      int Power;                                         // output variable
+
+   private:
+      int PrevSpeed; // last speed (in mm/sec)
+      int IError;    // integraded error (I-component of PID)
+};
+
+TMc McLeft, McRight;
 
 //-----------------------------------------------------------------------------
 // SpeedController - control motorspeed
 //-----------------------------------------------------------------------------
-// Parameters: SpeedSoll* (in mm/sec)
+// Parameters: TargetSpeed (in mm/sec)
 //-----------------------------------------------------------------------------
 void MotorController(int SetpointL, int SetpointR)
 {
-   int PError = 999;
-   int DError = 999;
-   int PowerL, PowerR;
+   McLeft.Compute( SetpointL, Position.ActSpeedL);
+   McRight.Compute(SetpointR, Position.ActSpeedR);
 
-   // convert setpoints from mm/s to ticks/interval
-   int SetpointL_Ticks = (SetpointL * (long) MAIN_TAKT_INTERVAL) / (F_ODO_TICK_TO_METRIC * 1024);
-   int SetpointR_Ticks = (SetpointR * (long) MAIN_TAKT_INTERVAL) / (F_ODO_TICK_TO_METRIC * 1024);
-
-  //printf("!! %d %d %d %d\n", SetpointL, SetpointL_Ticks, MAIN_TAKT_INTERVAL, ODO_TICK_TO_METRIC);
-
-   //----------------
-   // Left controller
-   //----------------
-   if (SetpointL_Ticks != 0) {
-      //  PID motor speed control:
-      PError = SetpointL_Ticks - Position.ActSpeedL;           // P error, in odo-pulsen per takt-inteval
-      DError = Position.ActSpeedL - Mc.PrevSpeedL;             // D error
-      Mc.PrevSpeedL = Position.ActSpeedL;
-      Mc.IErrorL += PError;                                    // I error
-
-      if (Mc.IErrorL >  MPID_I_MAX) Mc.IErrorL =  MPID_I_MAX;  //anti windup
-      if (Mc.IErrorL < -MPID_I_MAX) Mc.IErrorL = -MPID_I_MAX;
-
-      // Calculate PWM
-      PowerL = MPID_OFFSET       +                             // PID controller
-               SetpointL_Ticks   * MPID_SP_GAIN +
-               PError            * MPID_P_GAIN  +
-               Mc.IErrorL        * MPID_I_GAIN  +
-               DError            * MPID_D_GAIN  ;
-   } else {
-      Mc.IErrorL = 0;
-      PowerL     = 0;
-   }
-
-   //-----------------
-   // Right controller
-   //-----------------
-   if (SetpointR_Ticks != 0) {
-      //  PID motor speed control:
-      PError = SetpointR_Ticks - Position.ActSpeedR;           // P error, in odo-pulses per takt-inteval
-      DError = Position.ActSpeedR - Mc.PrevSpeedR;             // D error
-      Mc.PrevSpeedR = Position.ActSpeedR;
-      Mc.IErrorR += PError;                                    // I error
-
-      if (Mc.IErrorR >  MPID_I_MAX) Mc.IErrorR =  MPID_I_MAX;  //anti windup
-      if (Mc.IErrorR < -MPID_I_MAX) Mc.IErrorR = -MPID_I_MAX;
-
-      // Calculate PWM
-      PowerR = MPID_OFFSET       +                             // PID controller
-               SetpointR_Ticks   * MPID_SP_GAIN +
-               PError            * MPID_P_GAIN  +
-               Mc.IErrorR        * MPID_I_GAIN  +
-               DError            * MPID_D_GAIN  ;
-   } else {
-      Mc.IErrorR = 0;
-      PowerR     = 0;
-   }
-
-   Motors(PowerL, PowerR);
-//   printf("PID Soll: %d / %d, Ist: %d / %d, I: %d / %d, Power: %d / %d\n",
-//         SetpointL_Ticks, SetpointR_Ticks, Position.ActSpeedL, Position.ActSpeedR, Mc.IErrorL, Mc.IErrorR, PowerL, PowerR);
+   Motors(McLeft.Power, McRight.Power);
 }
-
