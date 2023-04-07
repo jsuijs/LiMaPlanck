9286230bda40a7ab990c387c80b000c56734b85d
Joep <jsuijs@gmail.com>
2023-04-07 16:56:39 +0200

    Brett Beauregard ' PID library added, ready for use.

--- LiMaPlanck/_Framework.cpp
+++ LiMaPlanck/_Framework.cpp
@@ -6,9 +6,11 @@
 #define FRAMEWORK_CODE
 
 #include "RobotSettings.h"
+#include "Libs/PID_v1.h"
 #include "Libs/MyRobot.h"  // this includes code with FRAMEWORK_CODE defined.
 #include "Project.h"
 
+#include "Libs/PID_v1.cpp"
 #include "Libs/MyRobot.cpp"
 #include "Libs/MotorController.cpp"
 #include "Libs/RcDispatch.cpp"
